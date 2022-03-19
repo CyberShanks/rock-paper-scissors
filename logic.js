bd27c654bdfb465da725bf4dd9b438b8e2569f03
Shashank <shashankmish27@gmail.com>
2022-03-19 12:16:56 +0530

    error in using querySelectorAll

--- logic.js
+++ logic.js
@@ -1,4 +1,20 @@
+console.log("Connected");
 console.log("Rock-Paper-Scissors");
+
+//BUG HERE, EVENT LISTENER NOT BEING ATTACHED PROPERLY
+// const buttNodes = document.querySelectorAll("button"); //queryselector here is bugged
+// console.log(buttNodes); //maybe error here
+/*Maybe the event script is processed before the webpage loads, thus it is not able to execute as it should... maybe asynchronous loading is required here */
+
+buttNodes.forEach((button) => {
+    button.addEventListener('click', () => {
+        const modal = document.querySelector('#modal-window');
+    modal.classList.add('showModal');
+    modal.classList.add('shadow');
+    });
+});
+
+
 
 // proper random function which works in a range specified by min max
 function properRandom(min, max) {
@@ -15,11 +31,13 @@ function computerPlay() {
 // game logic coded here, make playerSelection inclusive of all cases
 function logic(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
+
+    //player selects rock
     if (playerSelection == 'rock') {
-        if (computerSelection == 'Paper'){
+        if (computerSelection == 'Paper') {
             loss(playerSelection, computerSelection);
             return -1;
-        }    
+        }
         else if (computerSelection == 'Rock') {
             tie();
             return 0;
@@ -29,9 +47,9 @@ function logic(playerSelection, computerSelection) {
             return 1;
         }
     }
-
+    //player selects paper
     if (playerSelection == 'paper') {
-        if (computerSelection == 'Scissors'){
+        if (computerSelection == 'Scissors') {
             loss(playerSelection, computerSelection);
             return -1;
         }
@@ -44,9 +62,9 @@ function logic(playerSelection, computerSelection) {
             return 1;
         }
     }
-
+    //player selects scissors
     if (playerSelection == 'scissors') {
-        if (computerSelection == 'Rock'){
+        if (computerSelection == 'Rock') {
             loss(playerSelection, computerSelection);
             return -1;
         }
@@ -75,32 +93,19 @@ function loss(player, computer) {
     console.log("You Lose! " + computer + " beats " + player);
 }
 
-function game() {
-    let playerWin = 0;
-    let compWin = 0;
-    while (playerWin < 5 && compWin < 5) //logic here needs to be changed
-    {
-        //winCount = logic(prompt('Rock, Paper, Scissors?'), computerPlay());
-        if (winCount == 1)
-            playerWin++;
-        else if (winCount == -1)
-            compWin++;
-    }
-    if (playerWin == 5)
-        alert("You WIN!!!!");
-    else alert("You lose...");
-}
-game();
-
-// //coding the hover effect for buttons
-// function transition (button){
-//     const div = document.createElement('div');
-//     div.style.width = '0';
-//     div.style.height = '100%';
-//     button.appendChild(div);
+// function game() {
+//     let playerWin = 0;
+//     let compWin = 0;
+//     while (playerWin < 5 && compWin < 5) //logic here needs to be changed
+//     {
+//         //winCount = logic(prompt('Rock, Paper, Scissors?'), computerPlay());
+//         if (winCount == 1)
+//             playerWin++;
+//         else if (winCount == -1)
+//             compWin++;
+//     }
+//     if (playerWin == 5)
+//         alert("You WIN!!!!");
+//     else alert("You lose...");
 // }
-
-// const node = document.getElementsByName('button');
-// node.forEach(but => {
-//     but.addEventListener('mouseover', transition(but));
-// });
+// game();
